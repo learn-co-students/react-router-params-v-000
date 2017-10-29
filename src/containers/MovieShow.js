e4e625b008f963ab671879951c9a96bd5e3924b0
mikka <mikkanthrope@gmail.com>
2017-10-29 14:28:21 -0700

    Done.

--- src/containers/MovieShow.js
+++ src/containers/MovieShow.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const MovieShow = props =>
   <div>
-    <h3>Movie Show Component!</h3>
+    <h3>Title: {movie.title}</h3>
   </div>;
 
-export default MovieShow;
+const mapStateToProps = (state, ownProps) => {
+  const movie = state.movies.find(movie => movieId === ownProps.match.params.movieId)
+
+  if (movie) {
+    return { movie }
+  } else {
+    return { movie: {} }
+  }
+
+}
+
+
+export default connect(mapStateToProps)(MovieShow);
