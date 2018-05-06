83263d604ef2fdeea2c07985c3a8bd52ea7dd4ba
Igor Eskin <github email address>
2018-05-06 16:16:45 -0400

    Done.

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MoviesList = ({ movies }) => {
-  const renderMovies = movies.map(movie => 
-    <Link style={{ marginRight: '12px' }} key={movie.id} to={`/movies/${movie.id}`}>{movie.title}</Link>
+  const renderMovies = movies.map(movie =>
+    <Link style={{ marginRight: '12px' }} key={movie.id} to={`/movies/${movie.id}`}>{movie.title}<br /></Link>
   );
-  
+
   return (
     <div>
       {renderMovies}
--- src/containers/MovieShow.js
+++ src/containers/MovieShow.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const MovieShow = props =>
+const MovieShow = (props) => {
+  return (
   <div>
-    <h3>Movie Show Component!</h3>
-  </div>;
+    <h3>Title: {props.movie.title}</h3>
+  </div>
+)}
 
-export default MovieShow;
+const mapStateToProps = (state, ownProps) => {
+
+  const movie = state.movies.find(movie => movie.id === parseInt(ownProps.match.params.movieId, 10));
+
+  if (movie) {
+    return { movie }
+  } else {
+    return { movie: {} }
+  }
+}
+
+export default connect(mapStateToProps)(MovieShow);
